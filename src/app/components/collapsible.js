530733e6d14710bfb6f3388717f12f8a30b5ed6a
John Wong <john.wong@f5.com>
2024-09-18 04:41:14 +0000

    Adding ability to set title heading

--- src/app/components/collapsible.js
+++ src/app/components/collapsible.js
@@ -3,10 +3,10 @@
 
 import { Accordion, AccordionItem } from "@nextui-org/accordion";
 
-export function Collapsible({ title, children }) {
+export function Collapsible({ title, heading, children }) {
   return (
     <Accordion selectionMode="multiple">
-      <AccordionItem style={{ backgroundColor: 'lightgray' }} title={title}>
+      <AccordionItem style={{ backgroundColor: 'lightgray' }} title={title} HeadingComponent={heading}>
         {children}
       </AccordionItem>
     </Accordion>
